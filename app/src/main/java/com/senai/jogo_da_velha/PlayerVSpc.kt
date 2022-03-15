96d64376820364499ebcf741569f58afd14ae4cf
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2022-03-15 16:49:17 -0300

    tela inicial e do player vs pc realizada

---
+++ app/src/main/java/com/senai/jogo_da_velha/PlayerVSpc.kt
@@ -0,0 +1,11 @@
+package com.senai.jogo_da_velha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerVSpc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_vspc)
+    }
+}
